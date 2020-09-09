c52dc9d40019ec5ac54c77821186439610f4da35
Анна Гурова <aagurova_2@edu.hse.ru>
2020-09-10 00:32:35 +0500

    fixed CRUD operations for face

--- PhotoApi/Controllers/FaceController.cs
+++ PhotoApi/Controllers/FaceController.cs
@@ -20,32 +20,32 @@ namespace PhotoApi.Controllers
             _context = context;
         }
 
-        // GET: api/Face
+        // GET: api/person/{personId}/Face
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Face>>> GetFaces()
+        public async Task<ActionResult<IEnumerable<Face>>> GetFaces(int personId)
         {
-            return await _context.Faces.ToListAsync();
+            return await _context.Faces.Where(f=>f.PersonId==personId).ToListAsync();
         }
 
-        // GET: api/Face/5
+        // GET: api/person/{personId}/Face/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Face>> GetFace(int id)
+        public async Task<ActionResult<Face>> GetFace(int id, int personId)
         {
-            var face = await _context.Faces.FindAsync(id);
+            var face = await _context.Faces.Where(f => f.PersonId == personId && f.Id==id).ToListAsync();
 
             if (face == null)
             {
                 return NotFound();
             }
 
-            return face;
+            return new JsonResult(face);
         }
 
-        // PUT: api/Face/5
+        // PUT: api/person/{personId}/Face/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutFace(int id, Face face)
+        public async Task<IActionResult> PutFace(int id, Face face, int personId)
         {
             if (id != face.Id)
             {
@@ -73,19 +73,20 @@ namespace PhotoApi.Controllers
             return NoContent();
         }
 
-        // POST: api/Face
+        // POST: api/person/{personId}/Face
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<Face>> PostFace(Face face)
+        public async Task<ActionResult<Face>> PostFace(Face face, int personId)
         {
+            face.PersonId = personId;
             _context.Faces.Add(face);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetFace", new { id = face.Id }, face);
         }
 
-        // DELETE: api/Face/5
+        // DELETE: api/person/{personId}/Face/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<Face>> DeleteFace(int id)
         {
