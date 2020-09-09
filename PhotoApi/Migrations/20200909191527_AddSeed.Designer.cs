c62160b7b8202337dad0ceab0693ff6675d6b55c
Анна Гурова <aagurova_2@edu.hse.ru>
2020-09-10 00:32:35 +0500

    fixed CRUD operations for face

--- PhotoApi/Migrations/20200909191527_AddSeed.Designer.cs
+++ PhotoApi/Migrations/20200909191527_AddSeed.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PhotoApi.Models;
 
 namespace PhotoApi.Migrations
 {
     [DbContext(typeof(PhotoDbContext))]
-    partial class PhotoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200909191527_AddSeed")]
+    partial class AddSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
