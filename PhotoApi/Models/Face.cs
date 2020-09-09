a61551d8b84968c1a77147008ae097f0b64f2402
Анна Гурова <aagurova_2@edu.hse.ru>
2020-09-10 00:12:22 +0500

    CRUD operations on people and faces

--- PhotoApi/Models/Face.cs
+++ PhotoApi/Models/Face.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace PhotoApi.Models
         [Key]
         public int Id { get; set; }
         public int PersonId { get; set; }
-        public string  { get; set; }
+        public byte[] Photo { get; set; }
+        public virtual Person Person { get; set; }
     }
 }
--- PhotoApi/Models/PhotoDbContext.cs
+++ PhotoApi/Models/PhotoDbContext.cs
@@ -2,10 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace PhotoApi.Models
 {
-    public class PhotoDbContext
+    public class PhotoDbContext:DbContext
     {
+        public DbSet<Face> Faces { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Face>()
+                .HasIndex(f => f.Photo);
+            Seed(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-LOBOORA0\SQLEXPRESS;Initial Catalog=PhotoBase;Integrated Security=True");
+        }
+
+
+        private void Seed(ModelBuilder modelBuilder)
+        {
+            var firstPerson = new Person
+            {
+                Id = -1,
+                FirstName = "Anna",
+                LastName = "Gurova",
+                Patronymic = "Aleksandrovna"
+            };
+            var secondPerson = new Person
+            {
+                Id = -2,
+                FirstName = "Petr",
+                LastName = "Petrov",
+                Patronymic = "Petrovich"
+            };
+            modelBuilder.Entity<Person>().HasData(firstPerson, secondPerson);
+            modelBuilder.Entity<Face>().HasData(
+                new Face
+                {
+                    Id = -1,
+                    Photo = new byte[] { 1,1,1,1,1},
+                    PersonId = firstPerson.Id
+                },
+               new Face
+               {
+                   Id = -2,
+                   Photo = new byte[] { 1, 0, 1, 0, 1, 1 },
+                   PersonId = secondPerson.Id
+               }           
+            );
+        }
     }
 }
