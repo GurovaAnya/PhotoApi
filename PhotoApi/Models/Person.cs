119f190cee24323d0eed2726a300630f4721df26
Анна Гурова <aagurova_2@edu.hse.ru>
2020-09-13 23:51:47 +0500

    Gave attribute JsonInvisible to virtual fields

--- PhotoApi/Models/Person.cs
+++ PhotoApi/Models/Person.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace PhotoApi.Models
@@ -13,6 +14,7 @@ namespace PhotoApi.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Patronymic { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Face> Faces { get; set; }
 
     }
